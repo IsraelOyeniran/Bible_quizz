adea28e7268ef4956e4ebd82a7cab19d0f7e6862
Israel Oyeniran <122700893+IsraelOyeniran@users.noreply.github.com>
2023-10-06 02:10:05 -0230

    progress

--- Teen/carosel.js
+++ Teen/carosel.js
@@ -1,5 +1,15 @@
-const verse = "john 1:1";
-const url = "https://bible-api.com/".concat(verse);
-let response = await fetch(url);
-let data = await response.json()
-console.log(data.text);
+let curPage = 0;
+let caroselLink = ["Ephesians 4:32","jhskjhjakjah"]
+let boxBodys = ["Be kind to one another.","hjahjdkjhkjahhd","hjajajhadhhd"]
+
+
+function nextCard() {
+    curPage  += 1;
+    if (curPage >= 1){
+        curPage = 0
+    }
+    const cardBodys = document.getElementById("boxBodys");
+    cardBodys.textContent = "Be kind to one another.","Be kind to one another.";  
+    console.log(boxBodys);
+    console.log(CaroselText)
+}
