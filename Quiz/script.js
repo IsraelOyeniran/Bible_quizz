482d08905d31fd2faef56572844efe3e0b56b5ff
Israel Oyeniran <122700893+IsraelOyeniran@users.noreply.github.com>
2023-10-21 21:53:21 -0230

    progress

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,31 +1,61 @@
 const questions = [
     {
+        question: "Where was Jesus born?",
+        answers: [
+            {text: "(A) Nazareth", correct: false},
+            {text: "(B) Jerusalem", correct: false},
+            {text: "(C) Bethlehem", correct: true},
+            {text: "(D) Capernaum", correct: false},
+        ]
+    },
+    
+    {
         question: "What is the first book in the Bible?",
         answers: [
             {text: "(A) Matthew", correct: false},
             {text: "(B) Exodus", correct: false},
             {text: "(C) Genesis", correct: true},
-            {text: "(A) Jesus", correct: false},
+            {text: "(D) Jesus", correct: false},
         ]
     },
 
     {
-        question: "What four books tell about Jesus life on Earth?",
+        question: "How many disciples did Jesus have?",
         answers: [
-            {text: "(A) Matthew, Revelation, Mark, John", correct: false},
-            {text: "(B) Matthew, Mark, Luke, John", correct: true},
-            {text: "(C) Genesis, 1 John, Luke, Mark", correct: false},
-            {text: "(A) Matthew, Mark, Luke, 2 John", correct: false}, 
+            {text: "(A) 12", correct: true},
+            {text: "(B) 7", correct: false},
+            {text: "(C) 11", correct: false},
+            {text: "(D) 9", correct: false},
         ]
     },
 
     {
-        question: "What is the longest book in the Bible?",
+        question: "How did Jesus die?",
         answers: [
-            {text: "(A) Psalms", correct: true},
-            {text: "(B) Exodus", correct: false},
-            {text: "(C) Genesis", correct: false},
-            {text: "(A) Numbers", correct: false},
+            {text: "(A) He was flogged to death", correct: false},
+            {text: "(B) He was crucified", correct: true},
+            {text: "(C) He was stoned", correct: false},
+            {text: "(D) He was burned outside the city wall", correct: false},
+        ]
+    },
+
+    {
+        question: "What is the name of the disciple who betrayed Jesus?",
+        answers: [
+            {text: "(A) Simon Peter", correct: false},
+            {text: "(B) Andrew", correct: false},
+            {text: "(C) Bartholomew", correct: false},
+            {text: "(D) Judas Iscariot", correct: true}, 
+        ]
+    },
+
+    {
+        question: "Who gave Jesus gifts when he was born?",
+        answers: [
+            {text: "(A) The wise men", correct: true},
+            {text: "(B) The shepherds", correct: false},
+            {text: "(C) Mary", correct: false},
+            {text: "(D) The innkeeper", correct: false},
         ]
     },
 
@@ -35,27 +65,27 @@ const questions = [
             {text: "(A) Ezekiel", correct: false},
             {text: "(B) 1 Peter", correct: false},
             {text: "(C) Jude", correct: false},
-            {text: "(A) 3 John", correct: true},
+            {text: "(D) 3 John", correct: true},
         ] 
     },
 
     {
-        question: "How many books are in the Bible?",
+        question: "With what did Jesus feed 5000 people?",
         answers: [
-            {text: "(A) 52", correct: false},
-            {text: "(B) 70", correct: false},
-            {text: "(C) 50", correct: false},
-            {text: "(A) 66", correct: true},
+            {text: "(A) Corn and roast lamb", correct: false},
+            {text: "(B) Almonds and figs", correct: false},
+            {text: "(C) Herbs and fruit of the fields", correct: false},
+            {text: "(D) Loaves and fishes", correct: true},
         ] 
     },
 
     {
-        question: "How many books in the Old Testament?",
+        question: "What is the name of the prayer Jesus taught to his disciples?",
         answers: [
-            {text: "(A) 39 books in the OT", correct: true},
-            {text: "(B) 32 books in the OT", correct: false},
-            {text: "(C) 30 books in the OT", correct: false},
-            {text: "(A) 40 books in the OT", correct: false},
+            {text: "(A) The Righteous Prayer", correct: false},
+            {text: "(B) The Lord’s Prayer", correct: true},
+            {text: "(C) The Prayer of Peace", correct: false},
+            {text: "(D) The Prayer of Jesus", correct: false},
         ]  
     },
 
@@ -191,14 +221,46 @@ startQuiz();
 
 
 
+// {
+//     question: "What four books tell about Jesus life on Earth?",
+//     answers: [
+//         {text: "(A) Matthew, Revelation, Mark, John", correct: false},
+//         {text: "(B) Matthew, Mark, Luke, John", correct: true},
+//         {text: "(C) Genesis, 1 John, Luke, Mark", correct: false},
+//         {text: "(D) Matthew, Mark, Luke, 2 John", correct: false}, 
+//     ]
+// },
 
 
+// {
+//     question: "What is the longest book in the Bible?",
+//     answers: [
+//         {text: "(A) Psalms", correct: true},
+//         {text: "(B) Exodus", correct: false},
+//         {text: "(C) Genesis", correct: false},
+//         {text: "(A) Numbers", correct: false},
+//     ]
+// },
 
 
+// {
+//     question: "How many books are in the Bible?",
+//     answers: [
+//         {text: "(A) 52", correct: false},
+//         {text: "(B) 70", correct: false},
+//         {text: "(C) 50", correct: false},
+//         {text: "(A) 66", correct: true},
+//     ] 
+// },
 
 
-
-
-
-
+// {
+//     question: "How many books in the Old Testament?",
+//     answers: [
+//         {text: "(A) 39 books in the OT", correct: true},
+//         {text: "(B) 32 books in the OT", correct: false},
+//         {text: "(C) 30 books in the OT", correct: false},
+//         {text: "(D) 40 books in the OT", correct: false},
+//     ]  
+// },
    
