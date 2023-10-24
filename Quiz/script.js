1025ef01f180a08eaf5c839a92b409bf2d5f98ee
Israel Oyeniran <122700893+IsraelOyeniran@users.noreply.github.com>
2023-10-24 20:52:50 -0230

    update

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,213 +1,283 @@
 const questions = [
+    // {
+    //     question: "Where was Jesus born?",
+    //     answers: [
+    //         {text: "(A) Nazareth", correct: false},
+    //         {text: "(B) Jerusalem", correct: false},
+    //         {text: "(C) Bethlehem", correct: true},
+    //         {text: "(D) Capernaum", correct: false},
+    //     ]
+    // },
+
+    // {
+    //     question: "How many disciples did Jesus have?",
+    //     answers: [
+    //         {text: "(A) 12", correct: true},
+    //         {text: "(B) 7", correct: false},
+    //         {text: "(C) 11", correct: false},
+    //         {text: "(D) 9", correct: false},
+    //     ]
+    // },
+
+    // {
+    //     question: "How did Jesus die?",
+    //     answers: [
+    //         {text: "(A) He was flogged to death", correct: false},
+    //         {text: "(B) He was crucified", correct: true},
+    //         {text: "(C) He was stoned", correct: false},
+    //         {text: "(D) He was burned outside the city wall", correct: false},
+    //     ]
+    // },
+
+    // {
+    //     question: "What is the name of the disciple who betrayed Jesus?",
+    //     answers: [
+    //         {text: "(A) Simon Peter", correct: false},
+    //         {text: "(B) Andrew", correct: false},
+    //         {text: "(C) Bartholomew", correct: false},
+    //         {text: "(D) Judas Iscariot", correct: true}, 
+    //     ]
+    // },
+
+    // {
+    //     question: "Who gave Jesus gifts when he was born?",
+    //     answers: [
+    //         {text: "(A) The wise men", correct: true},
+    //         {text: "(B) The shepherds", correct: false},
+    //         {text: "(C) Mary", correct: false},
+    //         {text: "(D) The innkeeper", correct: false},
+    //     ]
+    // },
+
+    // {
+    //     question: "What is the shortest book in the Bible?",
+    //     answers: [
+    //         {text: "(A) Ezekiel", correct: false},
+    //         {text: "(B) 1 Peter", correct: false},
+    //         {text: "(C) Jude", correct: false},
+    //         {text: "(D) 3 John", correct: true},
+    //     ] 
+    // },
+
+    // {
+    //     question: "With what did Jesus feed 5000 people?",
+    //     answers: [
+    //         {text: "(A) Corn and roast lamb", correct: false},
+    //         {text: "(B) Almonds and figs", correct: false},
+    //         {text: "(C) Herbs and fruit of the fields", correct: false},
+    //         {text: "(D) Loaves and fishes", correct: true},
+    //     ] 
+    // },
+
+    // {
+    //     question: "What is the name of the prayer Jesus taught to his disciples?",
+    //     answers: [
+    //         {text: "(A) The Righteous Prayer", correct: false},
+    //         {text: "(B) The Lord’s Prayer", correct: true},
+    //         {text: "(C) The Prayer of Peace", correct: false},
+    //         {text: "(D) The Prayer of Jesus", correct: false},
+    //     ]  
+    // },
+
+    // {
+    //     question: "How many books in the New Testament?",
+    //     answers: [
+    //         {text: "(A) 49 books in the NT", correct: false},
+    //         {text: "(B) 34 books in the NT", correct: false},
+    //         {text: "(C) 27 books in the NT", correct: true},
+    //         {text: "(A) 20 books in the NT", correct: false},
+    //     ]  
+    // },
+
+    // {
+    //     question: "Which NT book has Jesus' Sermon on the Mount?",
+    //     answers: [
+    //         {text: "(A) Mark", correct: false},
+    //         {text: "(B) Judge", correct: false},
+    //         {text: "(C) James", correct: false},
+    //         {text: "(D) Matthew", correct: true},
+    //     ] 
+    // },
+
+    // {
+    //     question: "Who are the religious leaders who continually tried to trap Jesus with their questions?",
+    //     answers: [
+    //         {text: "(A) Pharisees", correct: true},
+    //         {text: "(B) Stoics", correct: false},
+    //         {text: "(C) Epicureans", correct: false},
+    //         {text: "(D) Galileans", correct: false},
+    //     ] 
+    // },
+
+    // {
+    //     question: "What did Jesus do for Lazarus?",
+    //     answers: [
+    //         {text: "(A) Cooked him dinner", correct: false},
+    //         {text: "(B) Took him for a walk", correct: false},
+    //         {text: "(C) Healed him from a fever", correct: false},
+    //         {text: "(D) Raised him from the dead", correct: true},
+    //     ] 
+    // },
+
+    // {
+    //     question: "What is the name of Jesus’ mother?",
+    //     answers: [
+    //         {text: "(A) Mary", correct: true},
+    //         {text: "(B) Salome", correct: false},
+    //         {text: "(C) Joanna", correct: false},
+    //         {text: "(D) Martha", correct: false},
+    //     ]  
+    // },
+
+    // {
+    //     question: "What job did Jesus’ earthly father, Joseph, do?",
+    //     answers: [
+    //         {text: "(A) Fisherman", correct: false},
+    //         {text: "(B) Carpenter", correct: true},
+    //         {text: "(C) Tentmaker", correct: false},
+    //         {text: "(D) Shephard", correct: false},
+    //     ]  
+    // },
+
+    // {
+    //     question: "What is the collective name of the stories Jesus told?",
+    //     answers: [
+    //         {text: "(A) Miracles", correct: false},
+    //         {text: "(B) Poems", correct: false},
+    //         {text: "(C) Parables", correct: true},
+    //         {text: "(D) Psalms", correct: false},
+    //     ]  
+    // },
+
+    // {
+    //     question: "Who wrote a lot of the letters to churches in the New Testament?",
+    //     answers: [
+    //         {text: "(A) Joseph", correct: false},
+    //         {text: "(B) Jesus", correct: false},
+    //         {text: "(C) Jairus", correct: false},
+    //         {text: "(D) Paul", correct: true},
+    //     ]  
+    // },
+
+    // {
+    //     question: "Who wanted to kill Jesus when he was a baby?",
+    //     answers: [
+    //         {text: "(A) Caesar", correct: false},
+    //         {text: "(B) John the Baptist", correct: false},
+    //         {text: "(C) The wise men", correct: false},
+    //         {text: "(D) Herod", correct: true},
+    //     ]  
+    // },
+
+    // {
+    //     question: "What did Simon Peter do for a living?",
+    //     answers: [
+    //         {text: "(A) Basket Maker", correct: false},
+    //         {text: "(B) Truck Driver", correct: false},
+    //         {text: "(C) Fisherman", correct: true},
+    //         {text: "(D) Trader", correct: false},
+    //     ]  
+    // },
+
+    // {
+    //     question: "What happened to Jesus after the resurrection?",
+    //     answers: [
+    //         {text: "(A) He went to live with Mary and Martha", correct: false},
+    //         {text: "(B) He made himself king", correct: false},
+    //         {text: "(C) He went to live in Jerusalem", correct: false},
+    //         {text: "(D) He ascended into heaven", correct: true},
+    //     ]  
+    // },
+
+    // {
+    //     question: "What did the Holy Spirit look like when the disciples received it?",
+    //     answers: [
+    //         {text: "(A) A rushing wind", correct: false},
+    //         {text: "(B) Tongues of fire", correct: true},
+    //         {text: "(C) White doves", correct: false},
+    //         {text: "(D) Billowing clouds", correct: false},
+    //     ]  
+    // },
+
     {
-        question: "Where was Jesus born?",
-        answers: [
-            {text: "(A) Nazareth", correct: false},
-            {text: "(B) Jerusalem", correct: false},
-            {text: "(C) Bethlehem", correct: true},
-            {text: "(D) Capernaum", correct: false},
-        ]
-    },
-    
-    {
-        question: "What is the first book in the Bible?",
+        question: "What was most likely the first Gospel written?",
         answers: [
             {text: "(A) Matthew", correct: false},
             {text: "(B) Exodus", correct: false},
-            {text: "(C) Genesis", correct: true},
-            {text: "(D) Jesus", correct: false},
+            {text: "(C) Mark", correct: true},
+            {text: "(D) Solomon", correct: false},
         ]
     },
 
     {
-        question: "How many disciples did Jesus have?",
+        question: "In what language was most of the New Testament given?",
         answers: [
-            {text: "(A) 12", correct: true},
-            {text: "(B) 7", correct: false},
-            {text: "(C) 11", correct: false},
-            {text: "(D) 9", correct: false},
+            {text: "(A) English", correct: false},
+            {text: "(B) Greek", correct: true},
+            {text: "(C) Spanish", correct: false},
+            {text: "(D) French", correct: false},
         ]
     },
 
     {
-        question: "How did Jesus die?",
+        question: "What was most likely the last Gospel written?",
         answers: [
-            {text: "(A) He was flogged to death", correct: false},
-            {text: "(B) He was crucified", correct: true},
-            {text: "(C) He was stoned", correct: false},
-            {text: "(D) He was burned outside the city wall", correct: false},
+            {text: "(A) Matthew", correct: false},
+            {text: "(B) Mark", correct: false},
+            {text: "(C) John", correct: true},
+            {text: "(D) Luke", correct: false},
         ]
     },
 
     {
-        question: "What is the name of the disciple who betrayed Jesus?",
+        question: "What was most likely the first of Paul's letters written",
         answers: [
-            {text: "(A) Simon Peter", correct: false},
-            {text: "(B) Andrew", correct: false},
-            {text: "(C) Bartholomew", correct: false},
-            {text: "(D) Judas Iscariot", correct: true}, 
+            {text: "(A) 1 Thessalonians", correct: true},
+            {text: "(B) Exodus", correct: false},
+            {text: "(C) Mark", correct: false},
+            {text: "(D) Solomon", correct: false},
         ]
     },
 
     {
-        question: "Who gave Jesus gifts when he was born?",
+        question: "Where did Jesus’ family go when Herod wanted to harm him as baby?",
         answers: [
-            {text: "(A) The wise men", correct: true},
-            {text: "(B) The shepherds", correct: false},
-            {text: "(C) Mary", correct: false},
-            {text: "(D) The innkeeper", correct: false},
+            {text: "(A) Church", correct: false},
+            {text: "(B) Jerusalem", correct: false},
+            {text: "(C) Rome", correct: false},
+            {text: "(D) Egypt", correct: true},
         ]
     },
 
     {
-        question: "What is the shortest book in the Bible?",
+        question: "How many of Jesus’ brothers are named in the Bible?",
         answers: [
-            {text: "(A) Ezekiel", correct: false},
-            {text: "(B) 1 Peter", correct: false},
-            {text: "(C) Jude", correct: false},
-            {text: "(D) 3 John", correct: true},
-        ] 
+            {text: "(A) Twelve (12)", correct: false},
+            {text: "(B) Four (4)", correct: true},
+            {text: "(C) None (0)", correct: false},
+            {text: "(D) Seven (7)", correct: false},
+        ]
     },
 
     {
-        question: "With what did Jesus feed 5000 people?",
+        question: "How many of Jesus’ sisters are named in the Bible?",
         answers: [
-            {text: "(A) Corn and roast lamb", correct: false},
-            {text: "(B) Almonds and figs", correct: false},
-            {text: "(C) Herbs and fruit of the fields", correct: false},
-            {text: "(D) Loaves and fishes", correct: true},
-        ] 
+            {text: "(A) Twelve (12)", correct: false},
+            {text: "(B) Four (4)", correct: false},
+            {text: "(C) None (0)", correct: true},
+            {text: "(D) Seven (7)", correct: false},
+        ]
     },
 
     {
-        question: "What is the name of the prayer Jesus taught to his disciples?",
+        question: "Name the place where Jesus walked on water?",
         answers: [
-            {text: "(A) The Righteous Prayer", correct: false},
-            {text: "(B) The Lord’s Prayer", correct: true},
-            {text: "(C) The Prayer of Peace", correct: false},
-            {text: "(D) The Prayer of Jesus", correct: false},
-        ]  
+            {text: "(A) Sea of Galilee", correct: true},
+            {text: "(B) Red Sea", correct: false},
+            {text: "(C) Atlantic Ocean", correct: false},
+            {text: "(D) Bethesda", correct: false},
+        ]
     },
-
-    {
-        question: "How many books in the New Testament?",
-        answers: [
-            {text: "(A) 49 books in the NT", correct: false},
-            {text: "(B) 34 books in the NT", correct: false},
-            {text: "(C) 27 books in the NT", correct: true},
-            {text: "(A) 20 books in the NT", correct: false},
-        ]  
-    },
-
-    {
-        question: "Which NT book has Jesus' Sermon on the Mount?",
-        answers: [
-            {text: "(A) Mark", correct: false},
-            {text: "(B) Judge", correct: false},
-            {text: "(C) James", correct: false},
-            {text: "(D) Matthew", correct: true},
-        ] 
-    },
-
-    {
-        question: "Who are the religious leaders who continually tried to trap Jesus with their questions?",
-        answers: [
-            {text: "(A) Pharisees", correct: true},
-            {text: "(B) Stoics", correct: false},
-            {text: "(C) Epicureans", correct: false},
-            {text: "(D) Galileans", correct: false},
-        ] 
-    },
-
-    {
-        question: "What did Jesus do for Lazarus?",
-        answers: [
-            {text: "(A) Cooked him dinner", correct: false},
-            {text: "(B) Took him for a walk", correct: false},
-            {text: "(C) Healed him from a fever", correct: false},
-            {text: "(D) Raised him from the dead", correct: true},
-        ] 
-    },
-
-    {
-        question: "What is the name of Jesus’ mother?",
-        answers: [
-            {text: "(A) Mary", correct: true},
-            {text: "(B) Salome", correct: false},
-            {text: "(C) Joanna", correct: false},
-            {text: "(D) Martha", correct: false},
-        ]  
-    },
-
-    {
-        question: "What job did Jesus’ earthly father, Joseph, do?",
-        answers: [
-            {text: "(A) Fisherman", correct: false},
-            {text: "(B) Carpenter", correct: true},
-            {text: "(C) Tentmaker", correct: false},
-            {text: "(D) Shephard", correct: false},
-        ]  
-    },
-
-    {
-        question: "What is the collective name of the stories Jesus told?",
-        answers: [
-            {text: "(A) Miracles", correct: false},
-            {text: "(B) Poems", correct: false},
-            {text: "(C) Parables", correct: true},
-            {text: "(D) Psalms", correct: false},
-        ]  
-    },
-
-    {
-        question: "Who wrote a lot of the letters to churches in the New Testament?",
-        answers: [
-            {text: "(A) Joseph", correct: false},
-            {text: "(B) Jesus", correct: false},
-            {text: "(C) Jairus", correct: false},
-            {text: "(D) Paul", correct: true},
-        ]  
-    },
-
-    {
-        question: "Who wanted to kill Jesus when he was a baby?",
-        answers: [
-            {text: "(A) Caesar", correct: false},
-            {text: "(B) John the Baptist", correct: false},
-            {text: "(C) The wise men", correct: false},
-            {text: "(D) Herod", correct: true},
-        ]  
-    },
-
-    {
-        question: "What did Simon Peter do for a living?",
-        answers: [
-            {text: "(A) Basket Maker", correct: false},
-            {text: "(B) Truck Driver", correct: false},
-            {text: "(C) Fisherman", correct: true},
-            {text: "(D) Trader", correct: false},
-        ]  
-    },
-
-    {
-        question: "What happened to Jesus after the resurrection?",
-        answers: [
-            {text: "(A) He went to live with Mary and Martha", correct: false},
-            {text: "(B) He made himself king", correct: false},
-            {text: "(C) He went to live in Jerusalem", correct: false},
-            {text: "(D) He ascended into heaven", correct: true},
-        ]  
-    },
-
-    {
-        question: "What did the Holy Spirit look like when the disciples received it?",
-        answers: [
-            {text: "(A) A rushing wind", correct: false},
-            {text: "(B) Tongues of fire", correct: true},
-            {text: "(C) White doves", correct: false},
-            {text: "(D) Billowing clouds", correct: false},
-        ]  
-    }
 ];
 
 const questionElement = document.getElementById("question");
@@ -364,4 +434,15 @@ startQuiz();
 //         {text: "(C) English", correct: false},
 //         {text: "(A) Spanish", correct: false},
 //     ]  
+// },
+
+
+// {
+//     question: "What is the first book in the Bible?",
+//     answers: [
+//         {text: "(A) Matthew", correct: false},
+//         {text: "(B) Exodus", correct: false},
+//         {text: "(C) Genesis", correct: true},
+//         {text: "(D) Jesus", correct: false},
+//     ]
 // },
