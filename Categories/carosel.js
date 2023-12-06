4aae8ed4e3eac848cadbb197eee394b38f209964
Israel Oyeniran <122700893+IsraelOyeniran@users.noreply.github.com>
2023-12-06 12:21:43 -0330

    update

--- Categories/carosel.js
+++ Categories/carosel.js
@@ -1,15 +1,9 @@
 let curPage = 0;
 let pageImages = ["photo_gallery/adult.jpg","photo_gallery/adult.jpg","photo_gallery/adult.jpg"]
-let pageBodys = [" to select your Bible study plan, if you are below 6 years or next if you are above.",
-                " Click here to select your Bible study plan, if you are below 20 years, or next if you are above.",
-                " Click here to select your Bible study plan, if you are  20 years and older or next to go back."
+let pageBodys = ["Click here to select your Bible study plan, if you are below 6 years, or next if you are above.",
+                "Click here to select your Bible study plan, if you are below 20 years, or next if you are above.",
+                "Click here to select your Bible study plan, if you are  20 years and older or next to go back."
                 ]
-
-
-// let pageBodys = ["Click here to select your Bible study plan, if you are below 6 years, or next if you are above.",
-//                 "Click here to select your Bible study plan, if you are below 20 years, or next if you are above.",
-//                 "Click here to select your Bible study plan, if you are  20 years and older or next to go back."
-//                 ]
 
 function alertCall() {
     alert("This is an alert");
@@ -25,11 +19,17 @@ function nextCard() {
     cardImg.src = pageImages[curPage];
 
     const cardText = document.getElementById("caroselText");
-    cardText.textContent = "";
+    cardText.textContent = ""; 
     cardText.textContent = pageBodys[curPage];
 
     const caroselBodyElement = document.getElementById("caroselBody");
     caroselBodyElement.innerHtml = carsolBodies[curPage];
+
+    const caroselHereElement = document.getElementById("caroselHere");
+    caroselHereElement.innerHtml = carsolHere[curPage];
+
+    const caroselNextElement = document.getElementById("caroselNext");
+    caroselNextElement.innerHtml = carsolNext[curPage];
 }
 
 
