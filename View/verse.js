d80cb27ac6c94aad541b2a32556ffef2cea688a8
Israel Oyeniran <122700893+IsraelOyeniran@users.noreply.github.com>
2024-02-02 16:34:43 -0330

    update

--- View/verse.js
+++ View/verse.js
@@ -38,41 +38,102 @@ versepairs = [
     ["Zz","Psalm 97:8,9"]
 ]
 
-function createVerseSelection(){
-
-    const caroselDiv = document.createElement("Div");
-    caroselDiv.id = "carosel"
-
-    const boxDiv = document.createElement("Div");
-    boxDiv.className = "box"
-
-    caroselDiv.appendChild(boxDiv)
-
-    const boxLettersDiv = document.createElement("Div")
-    boxLettersDiv.id = "boxLetters"
-
-    boxDiv.appendChild("boxLetters")
-
-    const boldedLettersDiv = document.createElement("b")
-    boldedLettersDiv.id = "boldedLetters"
-    boldedLettersDiv.innerHTML = "Aa"
-    boxLettersDiv.append(boldedLettersDiv)
-    
-    const navBarDiv = document.createElement("nav")
-    navBarDiv.id = "navBar"
-    boxDiv.appendChild(navBarDiv)
-
-    const verseContainer = getElementById("verseContainer")
-    verseContainer.appendChild(caroselDiv)  
-
-    console.log(verseContainer)
+async function getVerse(verseIndex){
+    let response = await fetch(
+        'https://bible-api.com/${versepairs[verseIndex][1]}'
+    );
+    let data = await response.json()
+    return data.text;
 }
 
-function createVerseSelection(){
-    versepairs.array.array.forEach(element => {
-        
+let parentElement = document.getElementById("carosel");
+const dialog = document.querySelector("dialog");
+const closeButton = document.querySelector("dialog button");
+let verseMessage = document.querySelector("dialog p");
+
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
+
+
+versepairs.forEach((versepair, index => {
+    let box = document.createElement("div");
+    box.className = "box";
+
+    let boxLetters = document.createElement("div");
+    boxLetters.className = "boxLetters";
+
+    let boldedLetters = document.createElement("b");
+    boldedLetters.className = "boldedLetters";
+    boldedLetters.textContent = versepair[0];
+
+    boxLetters.appendChild(boldedLetters);
+
+    let textDiv = document.createElement("div");
+    textDiv.className = "navBar";
+
+    let style = document.createElement("h1");
+    style.className = "style";
+
+    let caroselButton = document.createElement("button");
+    caroselButton.className = "caroselButton" + index;
+    caroselButton.textContent = versepair[1];
+
+    caroselButton.addEventListener("click", async () => {
+        verseMessage.innerHTML = await getVerse(index);
+        dialog.showModal();
     });
-}
+
+    style.appendChild(caroselButton);
+    
+    textDiv.appendChild(style);
+
+    box.appendChild(boxLetters);
+    box.appendChild(textDiv);
+
+    parentElement.appendChild(box);
+}));
+
+    
+
+
+
+
+// function createVerseSelection(){
+
+//     const caroselDiv = document.createElement("Div");
+//     caroselDiv.id = "carosel"
+
+//     const boxDiv = document.createElement("Div");
+//     boxDiv.className = "box"
+
+//     caroselDiv.appendChild(boxDiv)
+
+//     const boxLettersDiv = document.createElement("Div")
+//     boxLettersDiv.id = "boxLetters"
+
+//     boxDiv.appendChild("boxLetters")
+
+//     const boldedLettersDiv = document.createElement("b")
+//     boldedLettersDiv.id = "boldedLetters"
+//     boldedLettersDiv.innerHTML = "Aa"
+//     boxLettersDiv.append(boldedLettersDiv)
+    
+//     const navBarDiv = document.createElement("nav")
+//     navBarDiv.id = "navBar"
+//     boxDiv.appendChild(navBarDiv)
+
+//     const verseContainer = getElementById("verseContainer")
+//     verseContainer.appendChild(caroselDiv)  
+
+//     console.log(verseContainer)
+// }
+
+// function createVerseSelection(){
+//     versepairs.array.array.forEach(element => {
+        
+//     });
+// }
 
 
 
